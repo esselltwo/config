ba8f0b8268e3e95a61fd3dbbee1cbb58dbd72896
Calvin McPhail-Snyder <calvin@esselltwo.com>
2023-08-10 10:42:28 -0400

    Script for restricting tablet to one monitor

---
+++ tablet.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import subprocess
+import re
+
+desired_output = "HDMI-1"
+
+def get_stylus_id():
+    data = subprocess.run(["xsetwacom", "--list", "devices"], capture_output=True,text=True)
+
+    matches = re.findall("stylus.\sid: \d\d", data.stdout)
+
+    if len(matches) > 1:
+        raise RuntimeError("Too many styli found")
+    
+    return re.findall("\d\d", matches[0])[0]
+
+if __name__ == "__main__":
+    subprocess.run(["xsetwacom", "--set", get_stylus_id(), "MapToOutput", desired_output])

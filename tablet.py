28f8dd8064c70e7dd5861a41b741922b7a0fc36c
Calvin McPhail-Snyder <calvin@esselltwo.com>
2023-09-04 15:01:38 -0400

    Actually do regexes right

--- tablet.py
+++ tablet.py
@@ -7,12 +7,15 @@ desired_output = "HDMI-1"
 def get_stylus_id():
     data = subprocess.run(["xsetwacom", "--list", "devices"], capture_output=True,text=True)
 
-    matches = re.findall("stylus.\sid: \d\d", data.stdout)
+    matches = re.findall("stylus.\sid: \d+", data.stdout)
+
+    if len(matches) == 0:
+        raise RuntimeError("No stylus found")
 
     if len(matches) > 1:
         raise RuntimeError("Too many styli found")
-    
-    return re.findall("\d\d", matches[0])[0]
+   
+    return re.findall("\d+", matches[0])[0]
 
 if __name__ == "__main__":
     subprocess.run(["xsetwacom", "--set", get_stylus_id(), "MapToOutput", desired_output])

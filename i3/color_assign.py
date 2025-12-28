9c15802fab9af4948c1a8651be55bcccaae8f328
Calvin McPhail-Snyder <calvin@esselltwo.com>
2025-12-28 14:01:23 -0500

    Begin script for generating color files

--- i3/color_assign.py
+++ i3/color_assign.py
@@ -1,10 +1,14 @@
+#!/usr/bin/python3
+
+accent_color = "e31977"
+#accent_color = "a165bf"
+
+i3_color_config = """
 # colors
 set $text_light #ffffff
 set $text_dark #888888
 
-# set $focus #4c7899
-# purple to try?
-set $focus #a165bf
+set $focus #{}
 set $inact #5f676a
 set $unfoc #222222
 set $alert #900000
@@ -18,3 +22,22 @@ client.urgent           $alert  $alert  $text_light $alert    $alert
 client.placeholder      #000000 #0c0c0c $text_light #000000   #0c0c0c
 
 client.background       #ffffff
+""".format(accent_color)
+
+with open("colors.conf", "w") as i3_file:
+    i3_file.write(i3_color_config)
+
+polybar_color_config = """
+[colors]
+background = #222
+background-alt = #{}
+foreground = #dfdfdf
+foreground-alt = #555
+primary = #ffb52a
+secondary = #e60053
+alert = #bd2c40
+""".format(accent_color)
+
+with open("../polybar/colors.ini", "w") as polybar_file:
+    polybar_file.write(polybar_color_config)
+
--- polybar/README.md
+++ polybar/README.md
@@ -1,1 +1,3 @@
 Icons use the symbol-only Nerd Fonts from https://github.com/ryanoasis/nerd-fonts 
+
+Colors are generated using the script in the i3 folder

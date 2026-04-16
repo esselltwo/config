3727c2624143472e1993a3988ab15f3f73fb4706
Calvin McPhail-Snyder <calvin@esselltwo.com>
2026-04-16 09:08:24 -0400

    Typst preview keys match vimtex, add typst format

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -5,9 +5,16 @@ vim.lsp.config["tinymist"] = {
     filetypes = { "typst" },
 
     settings = {
-        -- ...
+        formatterMode = "typstyle",
+        formatterProseWrap = true, -- wrap lines in content mode
+        formatterPrintWidth = 80,  -- limit line length to 80 if possible
+        formatterIndentSize = 2,   -- indentation width
     }
 
 }
 
 vim.lsp.enable("tinymist")
+
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format({async = false})
+end)
--- nvim/lua/plugins/typst-preview.lua
+++ nvim/lua/plugins/typst-preview.lua
@@ -6,8 +6,8 @@ return {
     open_cmd = 'firefox %s --new-window -P typst-preview --class typst-preview'
   }, -- lazy.nvim will implicitly calls `setup {}`
   keys = {
-    { "<leader>l", "<cmd>TypstPreviewToggle<CR>", desc = "Toggle preview on/off" },
-    { "<leader>f", "<cmd>TypstPreviewFollowCursorToggle<CR>", desc = "Toggle following cursor in preview on/off" },
-    { "<leader>v", "<cmd>TypstPreviewSyncCursor<CR>", desc = "Jump to current cursor position in preview" },
+    { "<leader>ll", "<cmd>TypstPreviewToggle<CR>", desc = "Toggle preview on/off" },
+    { "<leader>lf", "<cmd>TypstPreviewFollowCursorToggle<CR>", desc = "Toggle following cursor in preview on/off" },
+    { "<leader>lv", "<cmd>TypstPreviewSyncCursor<CR>", desc = "Jump to current cursor position in preview" },
   }
 }

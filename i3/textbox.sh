908346edabaa8dcdb802bc1856331f94875e80d6
Calvin McPhail-Snyder <calvin@esselltwo.com>
2026-05-03 13:55:08 -0400

    Use neovide for clipboard editor

--- i3/textbox.sh
+++ i3/textbox.sh
@@ -3,6 +3,7 @@
 # from https://superuser.com/a/1177176
 
 # Dependencies: xclip, gvim
+# now: replace gvim with neovide (and nvim)
 # also i3-msg
 
 # Create an "ephemeral" file, that disappears with this process
@@ -16,6 +17,8 @@ ephemeral_path=/proc/$$/fd/3
 # Paste to the file
 xclip -selection clipboard -o > $ephemeral_path
 # Edit it. Make Vim respect the presence or lack of EOL at EOF
-gvim --class floating --nofork -c 'set nofixeol' $ephemeral_path
+#gvim --class floating --nofork -c 'set nofixeol' $ephemeral_path
+#kitty --class floating nvim -c 'set nofixeol' $ephemeral_path
+neovide --x11-wm-class floating --no-fork  $ephemeral_path -- -c 'set nofixeol'
 # Then copy it to the clipboard, removing the newline at the end
 xclip -selection clipboard < $ephemeral_path

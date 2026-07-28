fa3a5e5fefc8dbda3327b4755df1b292bca01219
Calvin McPhail-Snyder <calvin@esselltwo.com>
2026-07-28 14:03:13 -0400

    Better regex in tablet script

--- util-scripts/tablet.py
+++ util-scripts/tablet.py
@@ -2,20 +2,19 @@
 import subprocess
 import re
 
-desired_output = "DP-2"
+desired_output = "HDMI-1"
 
 def get_stylus_id():
     data = subprocess.run(["xsetwacom", "--list", "devices"], capture_output=True,text=True)
 
-    matches = re.findall("stylus.\sid: \d+", data.stdout)
+    ids = re.findall(r"stylus.*?id:\s*(\d+)", data.stdout)
 
-    if len(matches) == 0:
+    if len(ids) == 0:
         raise RuntimeError("No stylus found")
-
-    if len(matches) > 1:
+    if len(ids) > 1:
         raise RuntimeError("Too many styli found")
-   
-    return re.findall("\d+", matches[0])[0]
+
+    return ids[0]
 
 if __name__ == "__main__":
     subprocess.run(["xsetwacom", "--set", get_stylus_id(), "MapToOutput", desired_output])

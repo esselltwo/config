102a7bef383877ff486acaf320034fba7574b8e3
Calvin McPhail-Snyder <calvin@esselltwo.com>
2026-07-31 14:01:07 -0400

    Whoops, forgot a comma

--- nvim/lua/plugins/typst-preview.lua
+++ nvim/lua/plugins/typst-preview.lua
@@ -4,7 +4,7 @@ return {
         ft = "typst",
         version = '1.*',
         opts = {
-            open_cmd = 'firefox %s --new-window -P typst-preview --class typst-preview'
+            open_cmd = 'firefox %s --new-window -P typst-preview --class typst-preview',
             follow_cursor = false,
         }, -- lazy.nvim will implicitly calls `setup {}`
         keys = {

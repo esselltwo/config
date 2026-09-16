5f921f1b41c03352a93bdded2df6ecb003f66e1c
Calvin McPhail-Snyder <calvin@esselltwo.com>
2026-09-16 16:05:53 -0400

    basic fish config

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+alias eo='exo-open'
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias lg='lazygit'
